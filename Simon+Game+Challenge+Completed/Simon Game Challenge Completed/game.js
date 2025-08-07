b15d6ea59d3571d0ca948b9007622edae997b968
Khlood alsofyani <129340893+KhloodCS@users.noreply.github.com>
2025-08-07 21:01:34 +0300

    Add files via upload

---
+++ Simon+Game+Challenge+Completed/Simon Game Challenge Completed/game.js
@@ -0,0 +1,79 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {          // عشان يدخل الشرط مرة وحدة بعدها يطلع
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
+
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4); // generate a new color number 
+  var randomChosenColour = buttonColours[randomNumber]; // save the color from array
+  gamePattern.push(randomChosenColour); // push the color in the game pattern 
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100); // animation ..
+  playSound(randomChosenColour); 
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+// if the user mistakes .. restart .. 0 level .. empty array
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
